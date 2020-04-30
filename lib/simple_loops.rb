898caad29d0080e249027ccaadb792860243dbf5
Scott Singer <ssinger303@gmail.com>
2020-04-30 19:56:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,10 @@
 # Write your methods here
-def output_array_elements(array)
-  counter=0 
+#def output_array_elements(array)
+#counter=0 
   
-  while counter < array.length do
+  #while counter < array.length do
     puts array[counter]
     counter += 1 
   end
 end 
+
