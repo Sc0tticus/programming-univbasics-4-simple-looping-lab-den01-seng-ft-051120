0fa8489295d6197ed7c15d1778ae7782644177de
Scott Singer <ssinger303@gmail.com>
2020-04-30 20:04:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,12 +1,4 @@
 # Write your methods here
-#def output_array_elements(array)
-#counter=0 
-  
-  #while counter < array.length do
- #   puts array[counter]
-  #  counter += 1 
-  #end
-#end 
 def loop_message_five_times(message)
   counter=0 
   
